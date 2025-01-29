3f0bd9d0a9b25b75ba0e0b43efc6555ed8678d9f
codingbecca <131915898+codingbecca@users.noreply.github.com>
2025-01-29 11:27:38 -0800

    Part one

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 //Part 1
 // Select and cache the <main> element in a variable named mainEl.
+const mainEl = document.querySelector('main');
+
 // Set the background color of mainEl to the value stored in the --main-bg CSS custom property.
 // Hint: Assign a string that uses the CSS var() function like this: 'var(--main-bg)'.
+mainEl.style.backgroundColor = 'var(--main-bg)';
+
 // Set the content of mainEl to <h1>DOM Manipulation</h1>. There are a variety of ways to do this; pick whichever one that you think works best in this situation.
+mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
+
 // Add a class of flex-ctr to mainEl.
 // Hint: Use the Element.classList API.
+mainEl.classList.add('flex-ctr');
+
 
 //Part 2
 // Select and cache the <nav id="top-menu"> element in a variable named topMenuEl.
