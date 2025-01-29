124a79692f7b709ac19e7057cf9855c7a6124d5a
codingbecca <131915898+codingbecca@users.noreply.github.com>
2025-01-29 11:47:15 -0800

    Part three

--- index.js
+++ index.js
@@ -28,8 +28,21 @@ topMenuEl.style.backgroundColor = 'var(--top-menu-bg)'
 topMenuEl.classList.add('flex-around')
 
 //Part 3
+// Menu data structure
+var menuLinks = [
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
+  ];
 // Iterate over the entire menuLinks array and for each "link" object:
 // Create an <a> element.
 // On the new element, add an href attribute with its value set to the href property of the "link" object.
 // Set the new element's content to the value of the text property of the "link" object.
 // Append the new element to the topMenuEl element.
+menuLinks.forEach((link) => {
+    const anchorEl = document.createElement('a');
+    anchorEl.setAttribute('href', `${link.href}`);
+    anchorEl.textContent = `${link.text}`;
+    topMenuEl.append(anchorEl);
+});
