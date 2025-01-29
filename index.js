47d59c3b560f44352b8a5f37bb9edffc68a942b5
codingbecca <131915898+codingbecca@users.noreply.github.com>
2025-01-29 11:21:48 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+//Part 1
+// Select and cache the <main> element in a variable named mainEl.
+// Set the background color of mainEl to the value stored in the --main-bg CSS custom property.
+// Hint: Assign a string that uses the CSS var() function like this: 'var(--main-bg)'.
+// Set the content of mainEl to <h1>DOM Manipulation</h1>. There are a variety of ways to do this; pick whichever one that you think works best in this situation.
+// Add a class of flex-ctr to mainEl.
+// Hint: Use the Element.classList API.
+
+//Part 2
+// Select and cache the <nav id="top-menu"> element in a variable named topMenuEl.
+// Set the height of the topMenuEl element to be 100%.
+// Set the background color of topMenuEl to the value stored in the --top-menu-bg CSS custom property.
+// Add a class of flex-around to topMenuEl.
+
+//Part 3
+// Iterate over the entire menuLinks array and for each "link" object:
+// Create an <a> element.
+// On the new element, add an href attribute with its value set to the href property of the "link" object.
+// Set the new element's content to the value of the text property of the "link" object.
+// Append the new element to the topMenuEl element.
